7a644c50c195d0380552c7f3784fc62e973aa407
antonazzi <micheleantonazzi@gmail.com>
2024-05-14 10:27:10 +0200

    fixed launch file

---
+++ missions/launch/task/nav2_slam_launch.py
@@ -0,0 +1,68 @@
+# LAUNCH this file to run a SLAM node for generating an occupancy gridmap from laser scans.
+
+# USE THIS COMMAND TO SAVE TO FILE
+# ros2 run nav2_map_server map_saver_cli -f map_name
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation clock')
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("missions_pkg"),
+                                   'launch', 'nav2_slam_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+
+    start_async_slam_toolbox_node = Node(
+        parameters=[
+          slam_params_file,
+          {'use_sim_time': use_sim_time}
+        ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen'
+    )
+
+    start_map_server = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}]        
+    )
+
+    start_lifecicle_mamanager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time},
+                    {'autostart': True},
+                    {'node_names': ['map_server']}
+                    ]
+        )
+
+    # Add actions
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(start_async_slam_toolbox_node)
+    ld.add_action(start_map_server)
+    ld.add_action(start_lifecicle_mamanager)
+
+    return ld
