54ade52610dffc48573914ead947f49db5a4aa79
antonazzi <micheleantonazzi@gmail.com>
2024-05-16 15:48:11 +0200

    change namespace

--- missions/launch/giraff/giraff_launch.py
+++ missions/launch/giraff/giraff_launch.py
@@ -117,7 +117,7 @@ def generate_launch_description():
     return LaunchDescription([
         # Set env var to print messages to stdout immediately
         SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
-        DeclareLaunchArgument('namespace', default_value="giraff"),
+        DeclareLaunchArgument('namespace', default_value=NAMESPACE),
         DeclareLaunchArgument(
             "log_level",
             default_value=["info"],  #debug, info
