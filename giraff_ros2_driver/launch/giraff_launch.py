38b5b3d426543b88940dae3f48d2ce902d82a01a
antonazzi <micheleantonazzi@gmail.com>
2024-05-16 16:26:19 +0200

    fixed namespace

--- giraff_ros2_driver/launch/giraff_launch.py
+++ giraff_ros2_driver/launch/giraff_launch.py
@@ -1,10 +1,14 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, GroupAction, OpaqueFunction
 from launch.frontend.parse_substitution import parse_substitution
 
-def generate_launch_description():
+namespace = ''
+
+def launch_setup(context, *args, **kwargs):
+    namespace = LaunchConfiguration('namespace').perform(context)
+
     use_odom = DeclareLaunchArgument('publish_odom', default_value="False")
     giraff = Node (
         package='giraff_ros2_driver',
@@ -12,37 +16,41 @@ def generate_launch_description():
         name='giraff_node',
         output='screen',
         parameters=[
-        {'giraff_avr_port':'/dev/ttyS1'},
-        {'publish_odometry_over_tf':parse_substitution('$(var publish_odom)')},
-        {'publish_other_tf':True},
-        {'odom_topic':'odom'},
-        {'freq': 100.0},
-        {'verbose':False},
-        
-        {'base_frame_id': f'{parse_substitution("$(var namespace)")}_base_link'},
-        {'odom_frame_id': f'{parse_substitution("$(var namespace)")}_odom'},
-        {'stalk_frame_id': f'{parse_substitution("$(var namespace)")}_neck'},
-        {'head_frame_id': f'{parse_substitution("$(var namespace)")}_head'},
-        {'screen_frame_id': f'{parse_substitution("$(var namespace)")}_screen'},
-        {'base_footprint_frame_id': f'{parse_substitution("$(var namespace)")}_footprint'},
-        {'camera_frame_id': f'{parse_substitution("$(var namespace)")}_camera'},
-        {'laser_frame_id': f'{parse_substitution("$(var namespace)")}_laser_frame'},
-        
-        {'controller_mode':0},
-        {'max_linear_vel':0.7},
-        {'max_angular_vel':0.7},
-        {'linear_acceleration':0.2},
-        {'angular_acceleration':0.4},
-        {'virtual_gear_ratio':20.0},
-        {'tilt_bias':0.6},
-        {'battery_technology':'NIMH'},
-        {'battery_design_capacity':4.4},
-        {'battery_serial_number':'Giraff_battery'}
+            {'giraff_avr_port':'/dev/ttyS1'},
+            {'publish_odometry_over_tf':parse_substitution('$(var publish_odom)')},
+            {'publish_other_tf':True},
+            {'odom_topic':'odom'},
+            {'freq': 100.0},
+            {'verbose':False},
+
+            {'base_frame_id': f'{namespace}_base_link'},
+            {'odom_frame_id': f'{namespace}_odom'},
+            {'stalk_frame_id': f'{namespace}_neck'},
+            {'head_frame_id': f'{namespace}_head'},
+            {'screen_frame_id': f'{namespace}_screen'},
+            {'base_footprint_frame_id': f'{namespace}_footprint'},
+            {'camera_frame_id': f'{namespace}_camera'},
+            {'laser_frame_id': f'{namespace}_laser_frame'},
+
+            {'controller_mode':0},
+            {'max_linear_vel':0.7},
+            {'max_angular_vel':0.7},
+            {'linear_acceleration':0.2},
+            {'angular_acceleration':0.4},
+            {'virtual_gear_ratio':20.0},
+            {'tilt_bias':0.6},
+            {'battery_technology':'NIMH'},
+            {'battery_design_capacity':4.4},
+            {'battery_serial_number':'Giraff_battery'}
         ]
     )
 
+    return [GroupAction([use_odom, giraff])]
+
+def generate_launch_description():
+
+
     return LaunchDescription([
-        use_odom,
-        giraff,
         DeclareLaunchArgument('namespace', default_value='giraff')
+        OpaqueFunction(function=launch_setup)
     ])
