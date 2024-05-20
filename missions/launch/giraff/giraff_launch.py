ae63f57e0f306967d6fc33a4a01c586aef6105bb
antonazzi <micheleantonazzi@gmail.com>
2024-05-20 15:48:34 +0200

    fixed xacro

--- missions/launch/giraff/giraff_launch.py
+++ missions/launch/giraff/giraff_launch.py
@@ -40,7 +40,7 @@ def launch_setup(context, *args, **kwargs):
             launch_arguments={
                 'namespace': namespace,
                 'publish_odom': 'False',
-                'publish_other_tf': 'False'
+                'publish_other_tf': 'True'
             }.items()
 
         )
